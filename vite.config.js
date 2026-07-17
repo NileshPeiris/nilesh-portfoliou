b310e20b9a6750b97521e942f27279d537ee399a
Nilesh <nilesh@Nileshs-MacBook-Air.local>
2026-07-17 15:37:49 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change to '/repo-name/' for GitHub Pages
+  base: '/nilesh-portfoliou/',
 })
